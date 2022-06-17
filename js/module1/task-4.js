e6c147ed35d23f66808954e1ddec4ea891253168
Natasha Dashkovska <na.dashkovska@gmail.com>
2022-06-17 23:39:46 +0300

    add resolving of task 2, 3, 4

---
+++ js/module1/task-4.js
@@ -0,0 +1,45 @@
+// Задача 1-4
+// Использование if else и арифметических операторов
+// Напиши скрипт имитирующий вывод сообщений о списании средств на покупку товара.
+
+// Переменная credits хранит количество средств - 23580 кредитов.
+
+// Единица товара(дроида) стоит 3000 кредитов за
+// штуку и это значение хранится в переменной pricePerDroid.
+
+// Переменная orderPieces принимает значения null
+//     (отказ от покупок) и целое положительное число(количество единиц товара).
+
+// Переменная message будет выводить сообщение.В
+// эту переменную надо будет записать одно из трех сообщений о результате:
+
+// const CANCELED_BY_USER = 'Отменено пользователем!'
+// Вы купили ${} дроидов, на счету осталось ${} кредитов
+// const ACCESS_DENIED = 'Недостаточно средств на счету!'
+// Используйте вспомогательную переменную totalPrice,
+//     которая принимает рассчитанную общую цену заказа.
+// Используйте вспомогательную переменную balanceCredit,
+//     которая принимает разницу между credits и totalPrice.
+// А также используйте if else для выбора значений переменной message.
+
+const credits = 23580;
+const pricePerDroid = 3000;
+const orderPieces = null;
+
+let message;
+
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const ACCESS_DENIED = 'Недостаточно средств на счету!';
+
+const totalPrice = pricePerDroid * orderPieces;
+const balanceCredit = credits - totalPrice;
+
+if (orderPieces === null) {
+  message = CANCELED_BY_USER;
+} else if (balanceCredit < 0) {
+  message = ACCESS_DENIED;
+} else {
+  message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit} кредитов`;
+}
+
+console.log(message);
