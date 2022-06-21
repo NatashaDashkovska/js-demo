0c1b54cfb9101c54437acd16659387b0cff5558c
Natasha Dashkovska <na.dashkovska@gmail.com>
2022-06-21 21:27:39 +0300

    init branch 3

--- js/module1/task-12.js
+++
@@ -1,8 +0,0 @@
-// Объяви функцию sayHi, внутри которой добавь console.log() со строкой
-// "Hello, this is my first function!". После объявления вызови функцию sayHi.
-
-function sayHi() {
-  console.log('Hello, this is my first function!');
-}
-
-sayHi();
